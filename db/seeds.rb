4c2384cc87a9ae8235b4323ae4eb92cd77694dc4
Tristan Monteiro <monteirotristan@gmail.com>
2025-02-26 17:29:31 +0100

    Added the models plants and garden, validations etc..

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+Garden.destroy_all if Rails.env.development?
+
+Garden.create!(name: "Jardin des plantes", banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg")
+Garden.create!(name: "Jardin d'acclimation", banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg")
+
+puts "created #{Garden.count} gardens"
